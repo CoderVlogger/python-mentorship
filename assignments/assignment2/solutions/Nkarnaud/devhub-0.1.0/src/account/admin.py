b48c21954bd8aa2d93f2b23cff4415a2b85b5de7
Arnaud Mathias <arnaud.tsombeng@gmail.com>
2019-04-19 13:12:42 +0100

    registring an account

--- assignments/assignment2/solutions/Nkarnaud/devhub-0.1.0/src/account/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from account import models as account_models
-
-
-admin.site.register(account_models.Account, UserAdmin)
