857cc01090d6d126510832bddeb99eb9bac6ff2f
Arnaud Mathias <arnaud.tsombeng@gmail.com>
2019-04-19 13:29:26 +0100

    registring an account

--- assignments/assignment2/solutions/Nkarnaud/devhub-0.1.0/src/account/models.py
+++ assignments/assignment2/solutions/Nkarnaud/devhub-0.1.0/src/account/models.py
@@ -8,4 +8,4 @@ class Account(AbstractUser):
     More information here:
     https://docs.djangoproject.com/en/dev/topics/auth/customizing/#using-a-custom-user-model-when-starting-a-project
     """
-    REQUIRED_FIELDS = ['username', 'email']
+    REQUIRED_FIELDS = ['email']
