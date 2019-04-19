3121d3d0eaba88fddb58ee1b95c476e05ce02f82
Kanan Rahimov <mail@kenanbek.me>
2019-04-19 14:30:36 +0200

    Fix errors in assignment2/devhub

--- assignments/assignment2/devhub-0.1.0/src/devhub/settings.py
+++ assignments/assignment2/devhub-0.1.0/src/devhub/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'account.app.AccountConfig',
+    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
